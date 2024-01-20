f271017fedb331a2e30610271e4e830f2f0bd122
ArabRobo <121577164+ArabRobo@users.noreply.github.com>
2024-01-20 16:20:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ DB_URI = os.environ.get("DATABASE_URL", "postgres://bnztebfu:hrPXMvQ9F1RHEFB0cjz
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002021407169"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1002111336663"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
